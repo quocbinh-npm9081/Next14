185c998e051abc51305e117b6bc6b11790eaef7f
BinhQuoc <quocbinhgithub@gmail>
2024-10-04 11:29:06 +0700

    Reserved File names custom component and how to organize a nextjs project

--- app/page.js
+++ app/page.js
@@ -1,8 +1,11 @@
 import styles from "./page.module.css";
 import Link from "next/link";
+import Headers from "@/components/header";
+
 export default function Home() {
   return (
     <div className={styles.page}>
+        <Headers/> 
         <Link href="/about">about</Link>
     </div>
   );
